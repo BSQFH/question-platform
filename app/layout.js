f587e08713347a71b71e59c99b5299a3508c7de8
BSQFH <937962075@qq.com>
2026-07-18 21:48:48 +0800

    Add RootLayout component with metadata for title

---
+++ app/layout.js
@@ -0,0 +1,22 @@
+export const metadata = {
+  title: "在线题库"
+}
+
+
+export default function RootLayout({children}){
+
+  return (
+
+    <html lang="zh">
+
+      <body>
+
+        {children}
+
+      </body>
+
+    </html>
+
+  )
+
+}
