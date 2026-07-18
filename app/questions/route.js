c9411554a3cb1e87c4946161b0ddcd3bdd5674f0
BSQFH <937962075@qq.com>
2026-07-18 23:18:55 +0800

    Implement GET route for fetching questions

---
+++ app/questions/route.js
@@ -0,0 +1,25 @@
+import { createClient } from '@supabase/supabase-js'
+import { NextResponse } from 'next/server'
+
+
+const supabase = createClient(
+ process.env.NEXT_PUBLIC_SUPABASE_URL,
+ process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
+
+
+export async function GET(){
+
+ const {data,error}=await supabase
+ .from("questions")
+ .select("*")
+
+
+ if(error){
+   return NextResponse.json(error)
+ }
+
+
+ return NextResponse.json(data)
+
+}
