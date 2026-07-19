0ed0418745e8b4151a0f54a0d1c5d6a1d72031aa
BSQFH <937962075@qq.com>
2026-07-19 09:52:01 +0800

    Refactor Quiz component and enhance loading state

--- app/quiz/page.js
+++ app/quiz/page.js
@@ -2,77 +2,149 @@
 
 import { useEffect, useState } from "react"
 
+
 export default function Quiz(){
 
   const [questions,setQuestions] = useState([])
   const [index,setIndex] = useState(0)
   const [loading,setLoading] = useState(true)
+  const [result,setResult] = useState("")
+
 
   useEffect(()=>{
 
     fetch("/questions")
       .then(res=>res.json())
       .then(data=>{
+
         setQuestions(data)
         setLoading(false)
+
       })
 
   },[])
 
 
+
   if(loading){
+
     return (
+
       <main>
-        <h1>开始刷题</h1>
-        <p>题目加载中...</p>
+
+        <h1>
+          在线题库
+        </h1>
+
+        <p>
+          题目加载中...
+        </p>
+
       </main>
+
     )
+
   }
+
 
 
   const q = questions[index]
 
 
+
+  function checkAnswer(option){
+
+    if(option === q.answer){
+
+      setResult("✅ 回答正确")
+
+    }else{
+
+      setResult(
+        "❌ 回答错误，正确答案：" + q.answer
+      )
+
+    }
+
+  }
+
+
+
+  function nextQuestion(){
+
+    setResult("")
+
+    setIndex(index + 1)
+
+  }
+
+
+
   return (
+
     <main>
 
-      <h1>在线题库</h1>
+
+      <h1>
+        在线题库
+      </h1>
+
 
       <h2>
-        {q.title}
+        第 {index + 1} 题
       </h2>
+
+
+      <h3>
+        {q.title}
+      </h3>
+
 
       <p>
         {q.content}
       </p>
 
 
-      <button>
+
+      <button onClick={()=>checkAnswer("A")}>
         A. {q.option_a}
       </button>
 
-      <br/>
+      <br/><br/>
 
 
-      <button>
+      <button onClick={()=>checkAnswer("B")}>
         B. {q.option_b}
       </button>
 
-      <br/>
+      <br/><br/>
 
 
-      <button>
+      <button onClick={()=>checkAnswer("C")}>
         C. {q.option_c}
       </button>
 
-      <br/>
+      <br/><br/>
 
 
-      <button>
+      <button onClick={()=>checkAnswer("D")}>
         D. {q.option_d}
       </button>
 
 
+
+      <h3>
+        {result}
+      </h3>
+
+
+      <button onClick={nextQuestion}>
+        下一题
+      </button>
+
+
     </main>
+
   )
+
 }
